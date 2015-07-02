3f5ab9a01964536cecb8f6465c08606ff6bc3977
FooTearth <footearth@gmail.com>
2015-07-02 20:16:33 +0800

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-app='/root/footearth-blog/bin/gitbook.pm2.json'
+app_home='/root/footearth-blog'
+pm2_json="${app_home}/bin/gitbook.pm2.json"
 
 /bin/bash /root/.nvm/nvm.sh
 /bin/bash -l -c 'git clone https://github.com/footearth/footearth-blog.git'
+/bin/bash -l -c "cd ${app_home}"
 /bin/bash -l -c 'cnpm install'
 /bin/bash -l -c 'npm run clean'
 /bin/bash -l -c 'npm run build'
 /bin/bash -l -c 'pm2 dump'
-/bin/bash -l -c "pm2 startOrRestart ${app}"
+/bin/bash -l -c "pm2 startOrRestart ${pm2_json}"
 
 while true
 do
