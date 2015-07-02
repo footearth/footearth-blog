c96c30e7758236dac987cd607b4e294150475ad5
FooTearth <footearth@gmail.com>
2015-07-02 18:13:16 +0800

    adjust docker file && start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 app='/root/footearth-blog/bin/gitbook.pm2.json'
 
 /bin/bash /root/.nvm/nvm.sh
-/bin/bash -l -c 'git pull'
-/bin/bash -l -c 'npm install'
+/bin/bash -l -c 'git clone https://github.com/footearth/footearth-blog.git'
+/bin/bash -l -c 'cnpm install'
 /bin/bash -l -c 'npm run clean'
 /bin/bash -l -c 'npm run build'
 /bin/bash -l -c 'pm2 dump'
